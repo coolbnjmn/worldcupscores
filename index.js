03429433650823bfa3462b960ea555c45a2df57a
Benjamin Hendricks <bhendricks@linkedin.com>
2014-06-20 14:27:34 -0700

    added team names to json object pushed to web

--- index.js
+++ index.js
@@ -3,15 +3,55 @@ var logfmt = require("logfmt");
 var app = express();
 var request = require("request");
 
+var schedule = require("node-schedule");
+
+var global_scores;
+var rule = new schedule.RecurrenceRule();
+rule.minute = new schedule.Range(0,59, 1);
+
+request("http://worldcup.kimonolabs.com/api/matches?apikey=ab36d3439df5c4b72882313aff7e15ff", function(err, response, body) {
+	var jsonObj = JSON.parse(body);
+	for(var i = 0; i < jsonObj.length; i++) {
+		request("http://worldcup.kimonolabs.com/api/teams/"+jsonObj[i].homeTeamId+"?apikey=ab36d3439df5c4b72882313aff7e15ff", function(erri, responsei, bodyi) {
+			var teamObj = JSON.parse(bodyi);
+			for(var j = 0; j < jsonObj.length; j++) {
+				if(teamObj.id == jsonObj[j].homeTeamId) {
+					jsonObj[j].homeTeamId = teamObj;
+				} else if(teamObj.id == jsonObj[j].awayTeamId) {
+					jsonObj[j].awayTeamId = teamObj;
+				}
+			}
+			global_scores = JSON.stringify(jsonObj);
+		});
+	}
+	// global_scores = response;
+});
+schedule.scheduleJob(rule, function() {
+	request("http://worldcup.kimonolabs.com/api/matches?apikey=ab36d3439df5c4b72882313aff7e15ff", function(err, response, body) {
+		var jsonObj = JSON.parse(body);
+		for(var i = 0; i < jsonObj.length; i++) {
+			request("http://worldcup.kimonolabs.com/api/teams/"+jsonObj[i].homeTeamId+"?apikey=ab36d3439df5c4b72882313aff7e15ff", function(erri, responsei, bodyi) {
+				var teamObj = JSON.parse(bodyi);
+				for(var j = 0; j < jsonObj.length; j++) {
+					if(teamObj.id == jsonObj[j].homeTeamId) {
+						jsonObj[j].homeTeamId = teamObj;
+					} else if(teamObj.id == jsonObj[j].awayTeamId) {
+						jsonObj[j].awayTeamId = teamObj;
+					}
+				}
+				global_scores = JSON.stringify(jsonObj);
+			});
+		}
+		// global_scores = response;
+	});
+});
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  request("http://worldcup.kimonolabs.com/api/matches?apikey=ab36d3439df5c4b72882313aff7e15ff", function(err, response, body) {
-  res.send(response);
- });
+	res.send(global_scores);
 });
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
