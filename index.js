b8a60277f59539a3696d98731ed7959f83ee7038
Benjamin Hendricks <bhendricks@linkedin.com>
2014-06-20 10:26:17 -0700

    added kimono hook

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
+var request = require("request");
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  request("http://worldcup.kimonolabs.com/api/matches?apikey=ab36d3439df5c4b72882313aff7e15ff", function(err, response, body) {
+  res.send(response);
+ });
 });
 
 var port = Number(process.env.PORT || 5000);
