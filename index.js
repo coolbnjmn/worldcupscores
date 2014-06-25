6b1ab4ae262b71e3647b0004778166ddd8ae6316
Benjamin Hendricks <bhendricks@linkedin.com>
2014-06-25 13:52:17 -0700

    added players to match objects

--- index.js
+++ index.js
@@ -9,44 +9,19 @@ var global_scores;
 var rule = new schedule.RecurrenceRule();
 rule.minute = new schedule.Range(0,59, 1);
 
-request("http://worldcup.kimonolabs.com/api/matches?apikey=ab36d3439df5c4b72882313aff7e15ff", function(err, response, body) {
+request("http://worldcup.kimonolabs.com/api/matches?apikey=ab36d3439df5c4b72882313aff7e15ff", 
+	function(err, response, body) {
 	var jsonObj = JSON.parse(body);
 	for(var i = 0; i < jsonObj.length; i++) {
-		request("http://worldcup.kimonolabs.com/api/teams/"+jsonObj[i].homeTeamId+"?apikey=ab36d3439df5c4b72882313aff7e15ff", function(erri, responsei, bodyi) {
+		request("http://worldcup.kimonolabs.com/api/teams/"+jsonObj[i].homeTeamId+"?apikey=ab36d3439df5c4b72882313aff7e15ff", 
+			function(erri, responsei, bodyi) {
  			var teamObj;
 			try {
 				teamObj = JSON.parse(bodyi);
 			} catch(e) {
 				teamObj = null;
 			}
-			console.log(teamObj);
 			if(teamObj != null) {
-			for(var j = 0; j < jsonObj.length; j++) {
-				if(teamObj.id == jsonObj[j].homeTeamId) {
-					jsonObj[j].homeTeamId = teamObj;
-				} else if(teamObj.id == jsonObj[j].awayTeamId) {
-					jsonObj[j].awayTeamId = teamObj;
-				}
-			}
-			}
-			global_scores = JSON.stringify(jsonObj);
-		});
-	}
-	// global_scores = response;
-});
-schedule.scheduleJob(rule, function() {
-	request("http://worldcup.kimonolabs.com/api/matches?apikey=ab36d3439df5c4b72882313aff7e15ff", function(err, response, body) {
-		var jsonObj = JSON.parse(body);
-		for(var i = 0; i < jsonObj.length; i++) {
-			request("http://worldcup.kimonolabs.com/api/teams/"+jsonObj[i].homeTeamId+"?apikey=ab36d3439df5c4b72882313aff7e15ff", function(erri, responsei, bodyi) {
-	 			var teamObj;
-				try {
-					teamObj = JSON.parse(bodyi);
-				} catch(e) {
-					teamObj = null;
-				}
-				console.log(teamObj);
-				if(teamObj != null) {
 				for(var j = 0; j < jsonObj.length; j++) {
 					if(teamObj.id == jsonObj[j].homeTeamId) {
 						jsonObj[j].homeTeamId = teamObj;
@@ -54,13 +29,56 @@ schedule.scheduleJob(rule, function() {
 						jsonObj[j].awayTeamId = teamObj;
 					}
 				}
-				}
-				global_scores = JSON.stringify(jsonObj);
-			});
-		}
-		// global_scores = response;
-	});
+				request("http://worldcup.kimonolabs.com/api/players?apikey=ab36d3439df5c4b72882313aff7e15ff&teamId=" + teamObj.id, 
+					function(errplayers, responseplayers, bodyplayers) {
+						var playersObj;
+						try {
+							playersObj = JSON.parse(bodyplayers);
+						} catch(e) {
+							playersObj = null;
+						}
+						if(playersObj != null) {
+							for(var k = 0; k < jsonObj.length; k++) {
+								if(playersObj[0].teamId == jsonObj[k].homeTeamId.id) {
+									jsonObj[k].homeTeamPlayers = playersObj;
+								} else if(playersObj[0].teamId == jsonObj[k].awayTeamId.id) {
+									jsonObj[k].awayTeamPlayers = playersObj;
+								}
+							}
+						}
+						global_scores = JSON.stringify(jsonObj);
+				});
+			}
+			
+		});
+	}
 });
+// schedule.scheduleJob(rule, function() {
+// 	request("http://worldcup.kimonolabs.com/api/matches?apikey=ab36d3439df5c4b72882313aff7e15ff", function(err, response, body) {
+// 		var jsonObj = JSON.parse(body);
+// 		for(var i = 0; i < jsonObj.length; i++) {
+// 			request("http://worldcup.kimonolabs.com/api/teams/"+jsonObj[i].homeTeamId+"?apikey=ab36d3439df5c4b72882313aff7e15ff", function(erri, responsei, bodyi) {
+// 	 			var teamObj;
+// 				try {
+// 					teamObj = JSON.parse(bodyi);
+// 				} catch(e) {
+// 					teamObj = null;
+// 				}
+// 				if(teamObj != null) {
+// 				for(var j = 0; j < jsonObj.length; j++) {
+// 					if(teamObj.id == jsonObj[j].homeTeamId) {
+// 						jsonObj[j].homeTeamId = teamObj;
+// 					} else if(teamObj.id == jsonObj[j].awayTeamId) {
+// 						jsonObj[j].awayTeamId = teamObj;
+// 					}
+// 				}
+// 				}
+// 				global_scores = JSON.stringify(jsonObj);
+// 			});
+// 		}
+// 		// global_scores = response;
+// 	});
+// });
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
