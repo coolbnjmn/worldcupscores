e0f63baca3f2a1792ef7b0e59e996625406f3a62
Benjamin Hendricks <bhendricks@linkedin.com>
2014-06-26 09:24:06 -0700

    added scheduling

--- index.js
+++ index.js
@@ -53,32 +53,52 @@ request("http://worldcup.kimonolabs.com/api/matches?apikey=ab36d3439df5c4b728823
 		});
 	}
 });
-// schedule.scheduleJob(rule, function() {
-// 	request("http://worldcup.kimonolabs.com/api/matches?apikey=ab36d3439df5c4b72882313aff7e15ff", function(err, response, body) {
-// 		var jsonObj = JSON.parse(body);
-// 		for(var i = 0; i < jsonObj.length; i++) {
-// 			request("http://worldcup.kimonolabs.com/api/teams/"+jsonObj[i].homeTeamId+"?apikey=ab36d3439df5c4b72882313aff7e15ff", function(erri, responsei, bodyi) {
-// 	 			var teamObj;
-// 				try {
-// 					teamObj = JSON.parse(bodyi);
-// 				} catch(e) {
-// 					teamObj = null;
-// 				}
-// 				if(teamObj != null) {
-// 				for(var j = 0; j < jsonObj.length; j++) {
-// 					if(teamObj.id == jsonObj[j].homeTeamId) {
-// 						jsonObj[j].homeTeamId = teamObj;
-// 					} else if(teamObj.id == jsonObj[j].awayTeamId) {
-// 						jsonObj[j].awayTeamId = teamObj;
-// 					}
-// 				}
-// 				}
-// 				global_scores = JSON.stringify(jsonObj);
-// 			});
-// 		}
-// 		// global_scores = response;
-// 	});
-// });
+schedule.scheduleJob(rule, function() {
+	request("http://worldcup.kimonolabs.com/api/matches?apikey=ab36d3439df5c4b72882313aff7e15ff", 
+		function(err, response, body) {
+		var jsonObj = JSON.parse(body);
+		for(var i = 0; i < jsonObj.length; i++) {
+			request("http://worldcup.kimonolabs.com/api/teams/"+jsonObj[i].homeTeamId+"?apikey=ab36d3439df5c4b72882313aff7e15ff", 
+				function(erri, responsei, bodyi) {
+	 			var teamObj;
+				try {
+					teamObj = JSON.parse(bodyi);
+				} catch(e) {
+					teamObj = null;
+				}
+				if(teamObj != null) {
+					for(var j = 0; j < jsonObj.length; j++) {
+						if(teamObj.id == jsonObj[j].homeTeamId) {
+							jsonObj[j].homeTeamId = teamObj;
+						} else if(teamObj.id == jsonObj[j].awayTeamId) {
+							jsonObj[j].awayTeamId = teamObj;
+						}
+					}
+					request("http://worldcup.kimonolabs.com/api/players?apikey=ab36d3439df5c4b72882313aff7e15ff&teamId=" + teamObj.id, 
+						function(errplayers, responseplayers, bodyplayers) {
+							var playersObj;
+							try {
+								playersObj = JSON.parse(bodyplayers);
+							} catch(e) {
+								playersObj = null;
+							}
+							if(playersObj != null) {
+								for(var k = 0; k < jsonObj.length; k++) {
+									if(playersObj[0].teamId == jsonObj[k].homeTeamId.id) {
+										jsonObj[k].homeTeamPlayers = playersObj;
+									} else if(playersObj[0].teamId == jsonObj[k].awayTeamId.id) {
+										jsonObj[k].awayTeamPlayers = playersObj;
+									}
+								}
+							}
+							global_scores = JSON.stringify(jsonObj);
+					});
+				}
+
+			});
+		}
+	});
+});
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
